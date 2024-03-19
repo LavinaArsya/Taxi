ec90d594785ba5f985b216f7ee1ee82768729726
LavinaArsya <arsya@students.amikom.ac.id>
2024-03-19 21:27:13 +0700

    first commit

---
+++ taxi-5723/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace taxi_5723
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Taxi taxi = new Taxi();
+
+            taxi.Nama = "Jono";
+            taxi.Duty = true;
+            taxi.Id = 10;
+
+            taxi.orderan();
+            taxi.mengantar();
+            taxi.mengantar();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ taxi-5723/Taxi.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace taxi_5723
+{
+    internal class Taxi
+    {
+        public string Nama { get; set; }
+        public bool Duty { get; set; }
+        public float Id { get; set; }
+
+
+        public void orderan()
+        {
+            Console.WriteLine("Driver Name: {0}", Nama);
+            Console.WriteLine("On Duty: {0}", (Duty ? "Yes" : "No"));
+            Console.WriteLine("Number of Passenger: {0}\n", Id);
+        }
+
+        public void menjemput()
+        {
+            Console.WriteLine("Jono sedang menjemput penumpang");
+        }
+
+        public void mengantar()
+        {
+            Console.WriteLine("Jono selesai mengantar penumpang");
+        }
+    }
+}
